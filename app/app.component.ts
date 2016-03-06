95c0542f9e2b63f885db9c548be9b7c90c625aa4
Ahamed Imran <immy123@gmail.com>
2016-03-06 12:43:00 +0530

    fixed syntax errors

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import {Component} from 'angular2/core';
 @Component({
   selector: 'meetup-app',
   template: '<h1>Angular Meetup</h1>'
-});
+})
 
 export class AppComponent {
 
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,4 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
 
-boostrap(AppComponent);
+bootstrap(AppComponent);
