f2f58135185d28692d8ea5b07ef2d76d33af842b
Ahamed Imran <immy123@gmail.com>
2016-03-12 11:28:56 +0530

    implemented observable to get session data

--- app/session/session.service.ts
+++ app/session/session.service.ts
@@ -1,8 +1,22 @@
 import {Injectable} from 'angular2/core';
+import {Http, Response} from 'angular2/http';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class SessionService {
-  getSessions() {
+  constructor(private http: Http) {}
 
+  private _sessionsUrl = 'app/sessions';
+
+  getSessions() {
+    return this.http.get(this._sessionsUrl)
+                    .map(res => res.json().data)
+                    .do(data => console.log(data))
+                    .catch(this.handleError);
+  }
+
+  private handleError(error: Response) {
+    console.error(error);
+    return Observable.throw(error.json().error || 'Server error');
   }
 }
