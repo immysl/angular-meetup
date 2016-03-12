c636dcc823b88a446a2d614e5a5a5569ee69afc6
Ahamed Imran <immy123@gmail.com>
2016-03-12 21:53:56 +0530

    integrated dashboard with rest of the app

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,37 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+
 import {SessionService} from './session/session.service';
 import {SessionComponent} from './session/session.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
 @Component({
-  selector: 'app',
+  selector: 'meetup-app',
   template: `
     <h1>{{title}}</h1>
-    <sessions></sessions>
+    <a [routerLink]="['Sessions']">Sessions</a>
+    <router-outlet></router-outlet>
   `,
-  directives: [SessionComponent],
+  directives: [ROUTER_DIRECTIVES],
   providers: [
+    ROUTER_PROVIDERS,
     SessionService
   ]
 })
+
+@RouteConfig([
+  {
+    path: '/sessions',
+    name: 'Sessions',
+    component: SessionComponent
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  }
+])
 
 export class AppComponent {
   title = 'Angular Meetup';
