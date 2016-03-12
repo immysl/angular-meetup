3121d4badbe2e05b85293cd83ca3370153fb7191
Ahamed Imran <immy123@gmail.com>
2016-03-12 11:30:45 +0530

    imported all operators for Observable in main file

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,7 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
 
+// add all functionalities (e.g. map) to Observable
+import 'rxjs/Rx';
+
 bootstrap(AppComponent);
