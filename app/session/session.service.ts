d9469c97d9678ee0da421b33244f1cbecae2f025
Ahamed Imran <immy123@gmail.com>
2016-03-13 13:15:29 +0530

    fixed session service issues to return a session

--- app/session/session.service.ts
+++ app/session/session.service.ts
@@ -7,20 +7,21 @@ export class SessionService {
   constructor(private http: Http) {}
 
   private _sessionsUrl = 'app/sessions';
-  private _sessionUrl = 'app/session/';
 
   getSessions() {
     return this.http.get(this._sessionsUrl)
                     .map(res => res.json().data)
-                    .do(data => console.log(data))
+                    // .do(data => console.log(data))
                     .catch(this.handleError);
   }
 
   getSession(id: number) {
-    return this.http.get(this._sessionUrl)
-                    .map(res => res.json().data)
-                    .filter(session => session.id === id)
-                    .do(session => console.log(session))
+    return this.http.get(this._sessionsUrl)
+                    .map(res => {
+                      const sessions = res.json().data;
+                      const filteredSession = sessions.filter(session => session.id === id);
+                      return filteredSession[0];
+                    })
                     .catch(this.handleError);
   }
 
